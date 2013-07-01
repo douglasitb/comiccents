9bf1badd70fcb869a9c33e6ec775bfe198c8acb7
equipe2_3c <equipe2_3c@LAB1M06.itbeng.inf>
2013-07-01 09:46:38 -0300

    incluir produto

--- adm/frm_incluirProduto.php
+++ adm/frm_incluirProduto.php
@@ -15,10 +15,15 @@ validaAutenticacao("../logout.php", "../conexao/conecta.php", "ADM");?>
             
         Nome do Produto: <input type="text" name="nomeproduto"/></br></br>
         Descri&ccedil;&atilde;o do Produto: <input type="text" name="descricao"/></br></br>
-        Quantidade do Produto: <input type="text" name="quantidade"/></br></br>
-        Pre&ccedil;o do Produto: <input type="text" name="preco"/></br></br>
+        Pre&ccedil;o de Capa: <input type="text" name="preco"/></br></br>
         Pre&ccedil;o de Venda: <input type="text" name="precovenda"/></br></br>
-        Quantidade em Estoque:<input type="text" name="qtde_estoque"/></br></br>
+        Quantidade em Estoque:<input type="text" name="qtde_estoque"/>
+                    <select>
+                        <option>Excelente</option>
+                        <option>Bom</option>
+                        <option>Regular</option>
+                        <option>Péssimo</option>
+                    </select>  </br></br>
         Tamanho do Produto: <input type="text" name="tamanho_produto"/></br></br>
         N&uacute;mero de P&aacute;ginas: <input type="text" name="numero_pags"/></br></br>
         Estado F&iacute;sico: <input type="text" name="estado_fisico"/></br></br>
